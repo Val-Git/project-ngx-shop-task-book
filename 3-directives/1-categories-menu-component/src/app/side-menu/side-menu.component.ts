222a9948b8c5f886e914e8adc9958b366b85da47
Valentin Tkachev <val.sfinx@gmail.com>
2020-10-04 02:06:09 +0300

    feat: implemented menu

--- 3-directives/1-categories-menu-component/src/app/side-menu/side-menu.component.ts
+++ 3-directives/1-categories-menu-component/src/app/side-menu/side-menu.component.ts
@@ -1,7 +1,28 @@
-import { Component } from '@angular/core';
+import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {ICategory} from "../../../../../shared/mocks/3-directives/categories";
 
 @Component({
   selector: 'ngx-shop-side-menu',
   templateUrl: './side-menu.component.html',
 })
-export class SideMenuComponent {}
+export class SideMenuComponent {
+  @Input()
+  public categories: ICategory[] = [];
+
+  @Output()
+  public categoryChange: EventEmitter<string> = new EventEmitter();
+
+  public currentName: string | null = null;
+
+  public redirectTo(subCategoryName: string) {
+    this.categoryChange.emit(subCategoryName);
+  }
+
+  public hover(categoryName: string): void {
+    this.currentName = categoryName;
+  }
+
+  public mouseLeave(): void {
+    this.currentName = null;
+  }
+}
