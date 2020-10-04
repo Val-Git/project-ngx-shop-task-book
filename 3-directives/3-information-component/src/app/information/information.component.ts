035b90fdcc8fe35edc636c3e1aec71304121f66b
Valentin Tkachev <val.sfinx@gmail.com>
2020-10-04 17:25:37 +0300

    feat: implemented information component

--- 3-directives/3-information-component/src/app/information/information.component.ts
+++ 3-directives/3-information-component/src/app/information/information.component.ts
@@ -1,7 +1,24 @@
-import { Component } from '@angular/core';
+import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {IProduct} from "../../../../../shared/mocks/3-directives/product-information";
 
 @Component({
   selector: 'ngx-shop-information',
   templateUrl: './information.component.html',
 })
-export class InformationComponent {}
+export class InformationComponent {
+  @Input()
+  public product: IProduct = null;
+
+  @Output()
+  public addToCart = new EventEmitter();
+
+  public isShow = false;
+
+  public addToBasket() {
+    this.addToCart.emit();
+  }
+
+  public show(isShow: boolean) {
+    this.isShow = isShow;
+  }
+}
