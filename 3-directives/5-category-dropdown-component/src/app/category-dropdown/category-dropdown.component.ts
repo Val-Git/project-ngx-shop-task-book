9f94232624c3e836f7484cac3f3c7e0bde996e53
Valentin Tkachev <val.sfinx@gmail.com>
2020-10-08 02:12:00 +0300

    feat: implemented category menu

--- 3-directives/5-category-dropdown-component/src/app/category-dropdown/category-dropdown.component.ts
+++ 3-directives/5-category-dropdown-component/src/app/category-dropdown/category-dropdown.component.ts
@@ -1,7 +1,25 @@
-import { Component } from '@angular/core';
+import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {ICategory} from "../../../../../shared/mocks/3-directives/categories";
 
 @Component({
   selector: 'ngx-shop-category-dropdown',
   templateUrl: './category-dropdown.component.html',
 })
-export class CategoryDropdownComponent {}
+export class CategoryDropdownComponent {
+  @Output()
+  public subCategorySelectEvent: EventEmitter<string> = new EventEmitter();
+
+  @Input()
+  public categories: ICategory[] = [];
+
+  public currentIndex = null;
+  public currentCategory = '';
+
+  public subCategorySelect(subCategoryName: string) {
+    this.subCategorySelectEvent.emit(subCategoryName);
+  };
+
+  public showSubCategories(index: number) {
+    this.currentIndex = index;
+  }
+}
