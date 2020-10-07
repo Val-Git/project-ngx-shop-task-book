97d365c027cd83031a0f7311a0b4bc62a8ae14df
Valentin Tkachev <val.sfinx@gmail.com>
2020-10-08 01:24:04 +0300

    feat: implemented feedback component

--- 3-directives/4-description-component/src/app/description/feedbacks/feedbacks.component.ts
+++ 3-directives/4-description-component/src/app/description/feedbacks/feedbacks.component.ts
@@ -1,8 +1,20 @@
-import { Component } from '@angular/core';
+import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {IFeedback} from "../../../../../../shared/mocks/3-directives/product-information";
 
 @Component({
   selector: 'ngx-shop-feedbacks',
   templateUrl: './feedbacks.component.html',
   styleUrls: ['./feedbacks.component.sass'],
 })
-export class FeedbacksComponent {}
+export class FeedbacksComponent {
+
+  @Output()
+  public addFeedbackEvent: EventEmitter<string> = new EventEmitter();
+
+  @Input()
+  public feedbacks: IFeedback[] = [];
+
+  public addNewFeedback() {
+    this.addFeedbackEvent.emit('Some Feedback');
+  }
+}
